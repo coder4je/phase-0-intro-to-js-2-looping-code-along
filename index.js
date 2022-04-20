93c27b0a52c91532134c891c3db9bd99878a2434
Taemin Kim <taeminkimny@gmail.com>
2022-04-20 13:56:40 -0400

    commit tree 8ce3a7044d3cb8c9aead0cb3d721cfc8f0869c86

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+// let cards = [];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts); 
+
+
+function writeCards(names, adjective) {
+    let contents = [];  
+    for (let i=0; i < names.length; i++) {  
+        contents.push("Thank you, " + names[i] +", for the wonderful " + adjective + " gift!");
+
+    }   
+    return contents;    
+}
+
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+// writeCards([], []);
+
+function countDown(num) {
+    let i = num;
+    while (i > -1) {
+        console.log(i);
+        i--
+    }
+}
+
+
+countDown("10");
